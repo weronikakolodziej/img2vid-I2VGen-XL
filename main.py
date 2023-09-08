110109713560d9b2c4b79d74257fa5d5bfb04879
verix <weronikako@icloud.com>
2023-09-08 15:40:55 +0100

    adding files

---
+++ main.py
@@ -0,0 +1,8 @@
+from modelscope.pipelines import pipeline
+from modelscope.outputs import OutputKeys
+
+pipe = pipeline(task='image-to-video', model='damo/Image-to-Video', model_revision='v1.1.0')
+
+# IMG_PATH: your image path (url or local file)
+output_video_path = pipe(IMG_PATH, output_video='./output.mp4')[OutputKeys.OUTPUT_VIDEO]
+print(output_video_path)
