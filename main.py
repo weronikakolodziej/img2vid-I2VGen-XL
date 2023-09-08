12b7efa7568c874e8df9e0e7d6ead4879ec735ca
verix <weronikako@icloud.com>
2023-09-08 15:43:07 +0100

    jupyter installation update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from modelscope.pipelines import pipeline
 from modelscope.outputs import OutputKeys
 
+!pip install -r /content/sample_data/requirements.txt
+
 pipe = pipeline(task='image-to-video', model='damo/Image-to-Video', model_revision='v1.1.0')
 
 # IMG_PATH: your image path (url or local file)
